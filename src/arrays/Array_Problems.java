639b33481d29d508c5379b5870e14869a9bdf2c0
Suva Shahria <1uknow2@gmail.com>
2020-09-23 12:54:39 -0400

    fix

--- src/arrays/Array_Problems.java
+++ src/arrays/Array_Problems.java
@@ -1,6 +1,6 @@
-package array;
+package arrays;
 
-public class ArrayProblems {
+public class Array_Problems {
 
 	public static void main(String[] args) {
 		int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
